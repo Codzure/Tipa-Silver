01c425e944eb1f63c659ca5b0b7e917399e7763d
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2022-12-30 17:47:15 +0300

    Initial Commit

---
+++ Learning SwiftUI/Learning_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_SwiftUIApp.swift
+//  Learning SwiftUI
+//
+//  Created by Leonard Mutugi on 30/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Learning_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
