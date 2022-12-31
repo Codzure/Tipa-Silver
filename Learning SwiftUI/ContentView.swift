580cc96e0e273b639e1b2f8480606fd26c0f71c0
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2022-12-31 18:08:14 +0300

    Added tip %

--- Learning SwiftUI/ContentView.swift
+++ Learning SwiftUI/ContentView.swift
@@ -15,23 +15,43 @@ struct ContentView: View {
     let tipPercentages = [10, 20, 30, 40, 50, 0]
 
     var body: some View {
-        Form{
-            Section{
-                TextField("Amount", value: $checkAmount,format: .currency(code: Locale.current.currency?.identifier ?? "KES")
-                )
-                .keyboardType(.decimalPad)
-                
-                Picker("Number of People", selection: $numberOfPeople) {
-                    ForEach(0..<100) { numberOfPeople in
-                        Text("\(numberOfPeople) people")
+        NavigationView{
+            Form{
+                Section{
+                    
+                    TextField("Amount", value: $checkAmount,format: .currency(code: Locale.current.currency?.identifier ?? "KES")
+                    )
+                    .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(0..<100) { numberOfPeople in
+                            Text("\(numberOfPeople) people")
+                        }
                     }
+                    
+                    
+                }header: {
+                    Text("How much do you want to pay?")
+                    
                 }
                 
+                Section{
+                   
+                    Picker("Tip Percentage", selection: $tipPercent){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section{
+                    Text(checkAmount,format: .currency(code: Locale.current.currency?.identifier ?? "KES"))
+                }
             }
-            
-            Section{
-                Text(checkAmount,format: .currency(code: Locale.current.currency?.identifier ?? "KES"))
-            }
+            .navigationTitle("Tippa Silver")
         }
     }
 }
